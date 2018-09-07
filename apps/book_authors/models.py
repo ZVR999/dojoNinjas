d34bf4949fb1c71c0acfb0a8ef04868f3863e440
Zachkery <zachkery999@gmail.com>
2018-09-07 14:02:03 -0500

    New models/Many to many
    Created models: Book and Author.
    Added a many to many relationship on Author to connect the two models.

--- apps/book_authors/models.py
+++ apps/book_authors/models.py
@@ -4,3 +4,18 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    desc = models.TextField(1000)
+    created_at = models.DateTime(auto_now_add=True)
+    updated_at = models.DateTime(auto_now = True)
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    books = models.ManyToMany(Book, related_name='Authors')
+    email = models.CharField(max_length=255)
+    created_at = models.DateTime(auto_now_add=True)
+    updated_at = models.DateTime(auto_now = True)
+    
